ba14aef168021832cb27f7eb6031de77188d2d19
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2018-11-28 09:53:16 +0100

    work on substitutions

--- Context.hs
+++ Context.hs
@@ -10,18 +10,18 @@ type Entry w = (w,Exp w)
 type Context w = [Entry w]
 type Rule = Exp
 
-anyUnify :: Eq w => Enumerable v => Exp (v+w) -> Context w -> [Subst v w]
+anyUnify :: Eq w => Enumerable v => Exp (v+w) -> Context w -> [PSubs v w]
 anyUnify e c = do
   (x,e') <- c
   case unify2 e e' of
     Just s -> return s
     Nothing -> fail "oparst"
 
+
+-- v is the set of free variables introduced by the matching process.
+-- w are the variables of the context.
 ruleApplies :: Rule (v+w) -> Context w -> [Exp w]
 ruleApplies r [] = [r]
 ruleApplies (Pi v dom body) ctx = do
-  s <- anyUnify dom ctx
-  applySubst s $ _
-  -- map (_ s) ctx
-
-  -- ruleApplies body ctx
+  s <- anyUnify dom ctx -- see if the domain can be satisfied in the context
+  applySubst s (pushRight <$> body) $ \body' -> ruleApplies body' ctx
--- Unify2.hs
+++ Unify2.hs
@@ -10,7 +10,6 @@
 
 module Unify2 where
 import Expr
-type a + b = Either a b
 
 data Constraint v w where
   -- v : meta variables to unify
@@ -19,63 +18,55 @@ data Constraint v w where
   (:=:) :: forall v w z. Eq z => Exp (v+(z+w)) -> Exp (z+w) -> Constraint v w
 
 
-class Eq v => Enumerable v where
-  splitType :: v -> (forall w. Enumerable w => (v -> Next w) -> (Next w -> v) -> k) -> k
-  decide :: v -> Zero + v
-
 h1 :: Exp (Next (v + (z + w))) -> Exp (v + (Next z + w))
-h1 = fmap $ \case
-  There (Left x) -> Left x
-  There (Right (Right x)) -> Right (Right x)
-  There (Right (Left x)) -> Right (Left (There x))
-  Here s -> Right (Left (Here s))
+h1 = fmap (fmap pushLeft . pushRight)
 
 h2 :: Exp (Next (z + w)) -> Exp (Next z + w)
-h2 = fmap $ \case
-     (Here s) -> Left (Here s)
-     (There (Right x)) -> Right x
-     There (Left x) -> Left (There x)
+h2 = fmap pushLeft
 
 applyOneSubst :: forall v v' w. Eq v => (v -> Next v') -> Exp w -> Constraint v w ->Constraint v' w
 applyOneSubst d t (u :=: v) = (u >>= f) :=: v
   where f :: v + (z + w) -> Exp (v' + (z + w))
         f (Left x) = case d x of
-             Here _ -> (Right . Right) <$> t
+             Here -> (Right . Right) <$> t
              There x' -> V (Left x')
         f (Right w) = V (Right w)
 
-data MayS v t where
-  NothingS :: MayS v t
-  JustS :: (v -> Exp (v' + t)) -> MayS v t
+-- | A partial substitution from v to t. Unsubstituted variables are left in v'
+data PSubs v t where
+  PSubs :: Enumerable v' => (v' -> v) -> (v -> Exp (v' + t)) -> PSubs v t
 
+applySubst :: PSubs v w -> Exp (v+w) -> (forall v'. Exp (v'+w) -> k) -> k
+applySubst (PSubs _out subs) e k = k $ e >>= \case
+                                        Left x -> subs x
+                                        Right x -> V (Right x)
 
-unify :: forall v w. Enumerable v => Eq w => [Constraint v w] -> MayS v w
+unify :: forall v w. Enumerable v => Eq w => [Constraint v w] -> Maybe (PSubs v w)
 -- Invariant: the substitution is already applied
-unify [] = JustS (V . Left)
+unify [] = Just (PSubs id (V . Left))
 unify ((App args1 :=: App args2):constraints)
   | length args1 == length args2 = unify (zipWith (:=:) args1 args2++constraints) 
-  | otherwise = NothingS
+  | otherwise = Nothing
 unify ((Pi _ dom bod :=: Pi _ dom' bod'):constraints)
   = unify ((dom:=:dom'):(h1 bod :=:h2 bod'):constraints)
 unify ((V (Right x) :=: V x'):constraints)
   | x == x' = unify constraints
-  | x /= x' = NothingS
+  | x /= x' = Nothing
 unify ((V (Left x) :=: t):constraints) 
   -- | x `occursIn` t = Nothing
   -- | otherwise
   = splitType x $ \into outof -> case sequenceA t of
-      Left _ -> NothingS
-      Right t' -> case unify (map (applyOneSubst into t') constraints) of
-        NothingS -> NothingS
-        JustS f -> JustS $ \x' -> case into x' of
+      Left _ -> Nothing
+      Right t' -> flip fmap (unify (map (applyOneSubst into t') constraints)) $ \case
+        (PSubs outof' f) -> PSubs (outof . outof') $ \x' -> case into x' of
           There y -> f y
-          Here _ -> fmap Right t'
-unify (_:_) = NothingS
+          Here -> fmap Right t'
+unify (_:_) = Nothing
 
 -- | Identity (nothing is substituted)
 -- idSubst = M.empty
 
 
-unify2 :: forall v w. Eq w => Enumerable v => Exp (v+w) -> Exp w -> MayS v w
+unify2 :: forall v w. Eq w => Enumerable v => Exp (v+w) -> Exp w -> Maybe (PSubs v w)
 unify2 s t = unify @v @w [(:=:) @v @w @Zero ((Right <$>) <$> s)  (Right <$> t)]
 
