130d678913d9dde09cbdbba406497b2cf7c06baa
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2019-11-12 09:14:15 +0100

    fix build infrastructure

---
+++ .styx/shell.nix
@@ -0,0 +1,35 @@
+{ nixpkgs ? import <nixpkgs> {}
+
+ }:
+let nixpkgs_source =
+fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.09.tar.gz";
+  nixpkgs' = (import nixpkgs_source){};
+in with nixpkgs'.pkgs;
+let hp = haskellPackages.override{
+    overrides = self: super: {
+      pretty-compact = self.callPackage ./pretty-compact.nix {};
+      prolin = self.callPackage ./prolin.nix {};
+      };};
+     getHaskellDeps = ps: path:
+        let f = import path;
+            gatherDeps = { buildDepends ? [], libraryHaskellDepends ? [], executableHaskellDepends ? [], libraryToolDepends ? [], executableToolDepends ? [], ...}:
+               buildDepends ++ libraryHaskellDepends ++ executableHaskellDepends ++ libraryToolDepends ++ executableToolDepends;
+            x = f (builtins.intersectAttrs (builtins.functionArgs f)
+                                               (ps // 
+                                                nixpkgs'.pkgs) # can also depend on non-haskell packages
+                   // {stdenv = stdenv; mkDerivation = gatherDeps;});
+        in x;
+ghc = hp.ghcWithPackages (ps: with ps; stdenv.lib.lists.subtractLists
+[prolin]
+([ cabal-install 
+pretty-compact BNFC parsek
+  ]  ++ getHaskellDeps ps ./prolin.nix));
+in
+pkgs.stdenv.mkDerivation {
+  name = "my-haskell-env-0";
+  buildInputs = [ ghc ];
+  shellHook = ''
+ export LANG=en_US.UTF-8
+ eval $(egrep ^export ${ghc}/bin/ghc)
+'';
+}
