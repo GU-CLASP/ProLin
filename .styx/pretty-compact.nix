2f87bab6642de84f811b8465d5cea17b911d081c
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2020-07-03 11:12:15 +0200

    update ghc

--- .styx/pretty-compact.nix
+++ .styx/pretty-compact.nix
@@ -7,8 +7,8 @@ mkDerivation {
   version = "3.0";
   src = fetchgit {
     url = "https://github.com/jyp/prettiest.git";
-    sha256 = "04982rx0riv90nr80gicp80hvj1pf7zlmswicv13dhz6pphdsl72";
-    rev = "3ef1954f045b026c9842cac51549203370088120";
+    sha256 = "04lwkh21m7kjrf3bsra29a11qhyhs124h9w692xlzzbzdz4g1sws";
+    rev = "0fb7f1b2209d6763b9467c5aae2f952f8a929aed";
     fetchSubmodules = true;
   };
   libraryHaskellDepends = [ base base-compat containers ];
--- .styx/shell.nix
+++ .styx/shell.nix
@@ -2,12 +2,13 @@
 
  }:
 let nixpkgs_source =
-fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/nixos-20.03.tar.gz";
+fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/55668eb671b915b49bcaaeec4518cc49d8de0a99.tar.gz";
   nixpkgs' = (import nixpkgs_source){};
 in with nixpkgs'.pkgs;
 let hp = haskellPackages.override{
     overrides = self: super: {
       parsek = self.callPackage ./parsek.nix {};
+      pretty-compact = self.callPackage ./pretty-compact.nix {};
       prolin = self.callPackage ./prolin.nix {};
       };};
      getHaskellDeps = ps: path:
@@ -22,14 +23,14 @@ let hp = haskellPackages.override{
 ghc = hp.ghcWithPackages (ps: with ps; stdenv.lib.lists.subtractLists
 [prolin]
 ([ cabal-install 
-parsek BNFC pretty-compact
+parsek pretty-compact BNFC pretty-compact
   ]  ++ getHaskellDeps ps ./prolin.nix));
 in
 pkgs.stdenv.mkDerivation {
   name = "my-haskell-env-0";
   buildInputs = [ ghc ];
   shellHook = ''
- export LANG=C.UTF-8
+ export LANG=en_US.UTF-8
  eval $(egrep ^export ${ghc}/bin/ghc)
 '';
 }
